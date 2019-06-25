eaa115c0e5f8011c9b4544e26e8044fe71314d6e
Hayden Hastings <haydenhastings32@gmail.com>
2019-06-25 16:50:37 -0600

    Added model with ChatRoom struct.

---
+++ Firebase Chat/Firebase Chat/Model/ChatRoom.swift
@@ -0,0 +1,21 @@
+//
+//  ChatRoom.swift
+//  Firebase Chat
+//
+//  Created by Hayden Hastings on 6/25/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import MessageKit
+
+struct ChatRoom {
+    
+    var name: String?
+    var uid: String?
+    var timestamp: NSNumber?
+    var id: String?
+    
+}
+
